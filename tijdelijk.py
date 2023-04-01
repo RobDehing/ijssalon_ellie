ead9b50424aeaa88db9930f1ff32b92c1ee86dbe
RobDehing <rb.dhng@gmail.com>
2023-04-01 12:57:05 +0200

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -6,7 +6,7 @@ prijzen = {
 aanbieding = prijzen["aardbei"] * 0.8
 print(aanbieding)
 reclame_tekst = (f"Vandaag in de aanbieding: aardbeien-ijs, 1 liter - slechts € {aanbieding}")
-reclame_tekst2 = reclame_tekst[:63]
+reclame_tekst2 = reclame_tekst[:65]
 reclame_tekst3 = reclame_tekst2.upper()
 reclame_tekst4 = reclame_tekst3.split(" ")
 for el in reclame_tekst4:
