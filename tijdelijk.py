745a861834ca9bc26c0e9fa8dc91cfd8f12ef617
RobDehing <rb.dhng@gmail.com>
2023-04-01 12:40:49 +0200

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -3,9 +3,10 @@ prijzen = {
     "vanille" : 4,
     "chocolade" : 5
 }
-aanbieding = prijzen["vanille"] * 0.8 # mijn output is slechts 1 decimaal 3.2
-reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts € {aanbieding:.2f}") # voor 2 decimalen :.2f toegevoegd
-reclame_tekst2 = reclame_tekst[:63] # werkt als output aanbieding 3.20000000000000000000 is
+aanbieding = prijzen["aardbei"] * 0.8
+print(aanbieding)
+reclame_tekst = (f"Vandaag in de aanbieding: aardbeien-ijs, 1 liter - slechts € {aanbieding}")
+reclame_tekst2 = reclame_tekst[:63]
 reclame_tekst3 = reclame_tekst2.upper()
 reclame_tekst4 = reclame_tekst3.split(" ")
 for el in reclame_tekst4:
