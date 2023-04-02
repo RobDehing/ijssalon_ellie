a14455c9b8069a0b24f95353b051cfddb942ff2b
RobDehing <rb.dhng@gmail.com>
2023-04-02 15:02:15 +0200

    ijssalon_ellie update

--- reclame.py
+++ reclame.py
@@ -1,5 +1,8 @@
+from algemene_functies import mijn_functie_2
+
+
 def aanbieding_1(smaak, prijs, korting): 
-    uitvoer = f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {prijs - (prijs * korting)} euro."
+    uitvoer = f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs:.2f} euro voor {prijs - (prijs * korting):.2f} euro."
     return uitvoer
 
 def inkomsten_totaal(inkomsten, btw):
@@ -8,15 +11,26 @@ def inkomsten_totaal(inkomsten, btw):
     uitvoer = f"Het totaal van alle inkomsten van deze week is {totaal} euro, waarover {bedrag} euro btw betaald dient te worden."
     return uitvoer
 
-def low_and_high(my_list):
-    l_h = (min(my_list), max(my_list))
-    output = list(l_h)
-    return output
+def laag_en_hoog(mijn_lijst):
+    l_h = (min(mijn_lijst), max(mijn_lijst))
+    uitvoer = list(l_h)
+    return uitvoer
 
 def gemiddelde(mijn_lijst):
     gem_waarde = sum(mijn_lijst) / len(mijn_lijst)
-    return gem_waarde
+    uitvoer = f"De gemiddelde inkomsten deze week zijn {gem_waarde:.2f} euro."
+    return uitvoer
 
+def meervoudig(invoer_lijst):
+    uitvoer = laag_en_hoog(mijn_lijst = invoer_lijst)   
+    return uitvoer
+    
+
+
+def combinatie(invoer_lijst_2):
+    korte_lijst = laag_en_hoog(invoer_lijst_2)
+    uitvoer = mijn_functie_2(korte_lijst[0], korte_lijst[1])
+    return uitvoer
 
 
 
@@ -24,5 +38,7 @@ def gemiddelde(mijn_lijst):
 
 print(aanbieding_1("aardbei", 4, 0.1))      # nog op 2 decimalen
 print(inkomsten_totaal([220, 430, 125, 160, 205, 90, 345], 9))
-print(low_and_high([220, 430, 125, 160, 205, 90, 345]))
+print(laag_en_hoog([220, 430, 125, 160, 205, 90, 345]))
 print(gemiddelde([220, 430, 125, 160, 205, 90, 345]))
+print(meervoudig([10,5,3,2,1,2,9]))
+print(combinatie([2,10,5,8]))
